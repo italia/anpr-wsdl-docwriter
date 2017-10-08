409c843d907900e63b3862b970a0fe2025e95c65
Radeox <dawid.weglarz95@gmail.com>
2017-10-08 15:43:02 +0200

    Updated template, changed makefile and gitignore

--- bin/wsdl.sh
+++ bin/wsdl.sh
@@ -4,21 +4,16 @@ DIR=$(pwd)
 NAME="*.wsdl"
 TMP_FILE="/tmp/wsdl_tmp"
 
-# for d in ../wsdl/*; do
 for d in wsdl/*; do
 	FILE=$DIR/$d/$NAME
 
-    # echo $DIR/$d
-    # if [ -d "$DIR/$d" ]; then
-    # 	exit 1
-    # fi
-
-    if [ ${FILE: -4} == ".xsd" ]; then 
-        cp $DIR/bin/wsdl-viewer/wsdl-viewer.xsl $DIR/$d
+    # Check if file is wsdl and if true add html render (italia-wsdl-viewer.xsl)
+    if [ ${FILE: -5} == ".wsdl" ]; then 
+        cp $DIR/bin/wsdl-viewer/italia-wsdl-viewer.xsl $DIR/$d
         sed 's/<?xml version="1.0" encoding="UTF-8"?>//g' $FILE > $TMP_FILE
-        sed -i '1s/^/<?xml version="1.0" encoding="UTF-8"?> <?xml-stylesheet type="text\/\xsl" href="wsdl-viewer.xsl"?> /' $TMP_FILE
-        mv $TMP_FILE $FILE
+        sed -i '1s/^/<?xml version="1.0" encoding="UTF-8"?> <?xml-stylesheet type="text\/\xsl" href="italia-wsdl-viewer.xsl"?> /' $TMP_FILE
+        mv $TMP_FILE '$FILE.xml'
     fi 
-	#sed 's/.\{38\}/&<?xml-stylesheet type="text\/\xsl" href="wsdl-viewer.xsl"?>/' $FILE > $TMP_FILE
-	#sed 's/.*<?xml version="1.0" encoding="UTF-8"?>.*/&\n<?xml-stylesheet type="text\/\xsl" href="wsdl-viewer.xsl"?>/' $FILE > $TMP_FILE
+	# sed 's/.\{38\}/&<?xml-stylesheet type="text\/\xsl" href="wsdl-viewer.xsl"?>/' $FILE > $TMP_FILE
+	# sed 's/.*<?xml version="1.0" encoding="UTF-8"?>.*/&\n<?xml-stylesheet type="text\/\xsl" href="wsdl-viewer.xsl"?>/' $FILE > $TMP_FILE
 done
